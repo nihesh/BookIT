5acf88ac097075eec723f9cdb26b9c3870c8c51b
nihesh <nihesh16059@iiitd.ac.in>
2017-10-11 01:14:47 +0530

    Hover functionality added to Reservation package

--- src/Reservation/ReservationGUIController.java
+++ src/Reservation/ReservationGUIController.java
@@ -4,5 +4,39 @@
 
 package Reservation;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+
+
 public class ReservationGUIController {
+    @FXML
+    private StackPane HoverPane;
+    @FXML
+    private Label RoomNo;
+    public void showReadOnlyBookings(){
+        RoomNo.setText("C01");
+        try {
+            Thread.sleep(100);
+        }
+        catch(Exception e){
+            System.out.println("Error in ReservationGUIController: ShowReadOnlyBookings");
+        }
+        HoverPane.setVisible(true);
+        HoverPane.setDisable(true);
+        HoverPane.setOpacity(0.95);
+    }
+    public void exitReadOnlyBookings(){
+        try {
+            Thread.sleep(100);
+        }
+        catch(Exception e){
+            System.out.println("Error in ReservationGUIController: ShowReadOnlyBookings");
+        }
+        HoverPane.setVisible(false);
+        HoverPane.setDisable(false);
+        HoverPane.setOpacity(1);
+        RoomNo.setText("Not Set");
+    }
 }
