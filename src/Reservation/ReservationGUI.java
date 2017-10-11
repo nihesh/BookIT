e2813351ff06c10d9de285f9f69a907c98426f50
nihesh <nihesh16059@iiitd.ac.in>
2017-10-12 00:24:09 +0530

    Logo added to the project and applied to ReservationGUI

--- src/Reservation/ReservationGUI.java
+++ src/Reservation/ReservationGUI.java
@@ -8,13 +8,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class ReservationGUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Reservation.fxml"));
+        File file = new File("./src/BookIT_icon.jpg");
+        primaryStage.getIcons().add(new Image(file.toURI().toString()));
         primaryStage.setTitle("BookIT");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 1920, 1000));
--- src/Reservation/ReservationGUIController.java
+++ src/Reservation/ReservationGUIController.java
@@ -9,15 +9,20 @@ import javafx.animation.ParallelTransition;
 import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.event.Event;
 import javafx.util.Duration;
 
-import java.awt.print.Book;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class ReservationGUIController {
+public class ReservationGUIController implements Initializable{
     private int appearAfter_HoverPane = 200;
     @FXML
     private StackPane HoverPane;
@@ -27,6 +32,14 @@ public class ReservationGUIController {
     private Button BackBtn;
     @FXML
     private Button BookBtn;
+    @FXML
+    private ImageView logo;
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        File file = new File("./src/BookIT_logo.jpg");
+        Image image = new Image(file.toURI().toString());
+        logo.setImage(image);
+    }
     private void induceDelay(long time){
         try {
             Thread.sleep(time);
