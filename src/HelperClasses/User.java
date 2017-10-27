ea7deb8f1c8dbca8301e46761391123260ce749c
HarshPathakhp <harsh16041@iiitd.ac.in>
2017-10-27 22:51:27 +0530

    helper classes started

---
+++ src/HelperClasses/User.java
@@ -0,0 +1,25 @@
+
+package HelperClasses;
+
+import java.util.Date;
+
+public class User {
+	private String Name;
+	private String Password; 	//User account password
+	private Email Email; 		//Email of user
+	private String Usertype;	//Student, Faculty and Admin
+	public boolean Authenticate(String password) {
+		
+	}
+	public boolean ChangePassword(String oldPassword, String newPassword) {
+		
+	}
+	public void serialize() {
+		
+	}
+	public void logout() {
+		
+	}
+	
+	
+}
