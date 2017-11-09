1689fefae9a19a5237f6d738800f41dcb7f4ce1f
HarshPathakhp <harsh16041@iiitd.ac.in>
2017-11-09 20:30:50 +0530

    post conditions added

---
+++ src/HelperClasses/BookIT.java
@@ -0,0 +1,12 @@
+package HelperClasses;
+
+public class BookIT {
+ User ActiveUser=null;
+ public BookIT(User u) {
+	// TODO Auto-generated constructor stub
+	 ActiveUser=u;
+ }
+ public boolean login() {
+	 return true;
+ }
+}
