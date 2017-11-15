568d1a906b52984fea88622b5e4687319af261b1
nihesh <nihesh16059@iiitd.ac.in>
2017-11-15 16:18:54 +0530

    Added support for both 1080p and 720p screens

--- src/LoginSignup/LoginSignupGUI.java
+++ src/LoginSignup/LoginSignupGUI.java
@@ -10,15 +10,28 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 
+import java.awt.*;
 import java.io.File;
 
 public class LoginSignupGUI extends Application{
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
-			Parent root = FXMLLoader.load(getClass().getResource("LoginSignup1080p.fxml"));
-			Scene scene = new Scene(root,850,567);
+			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			double Screenwidth = screenSize.getWidth();
+			Parent root;
+			int width,height;
+			if(Screenwidth>=1920) {
+				root = FXMLLoader.load(getClass().getResource("LoginSignup1080p.fxml"));
+				width=850;
+				height=567;
+			}
+			else{
+				root = FXMLLoader.load(getClass().getResource("LoginSignup.fxml"));
+				width=600;
+				height=400;
+			}
+			Scene scene = new Scene(root,width,height);
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
 			File file = new File("./src/BookIT_icon.jpg");
