9538253e567e392ffd59656fda90d416ff6ebe98
nihesh <nihesh16059@iiitd.ac.in>
2017-12-26 15:40:10 +0530

    bugs fixed

--- src/AdminReservation/NotifyController.java
+++ src/AdminReservation/NotifyController.java
@@ -1,5 +1,18 @@
 package AdminReservation;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class NotifyController {
-	
+    @FXML
+    private StackPane notificationPane;
+    public void initialize(URL location, ResourceBundle resources){
+        System.out.println("hi");
+        Label l = new Label();
+        notificationPane.getChildren().add(l);
+    }
 }
