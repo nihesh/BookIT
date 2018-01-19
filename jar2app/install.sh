babd137def3e841aaee4d07defa8a6f01f801db3
nihesh <nihesh16059@iiitd.ac.in>
2018-01-19 19:06:52 +0530

    OSX executable added

---
+++ jar2app/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -z "$1" ]
+  then
+    PREFIX=/usr/bin
+  else
+    PREFIX=$1
+fi
+
+if [ ! -e  $PREFIX/jar2app_basefiles ]
+then
+  mkdir $PREFIX/jar2app_basefiles
+fi
+cp -r jar2app_basefiles/* $PREFIX/jar2app_basefiles
+chmod -R a+rw $PREFIX/jar2app_basefiles
+cp jar2app $PREFIX
+chmod +x $PREFIX/jar2app
+
